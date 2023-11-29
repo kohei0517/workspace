2a7bf688c52367b8926bfcdfc731ae6dbfedf02e
kohei0517 <losthound.kei127@gmail.com>
2023-11-29 21:43:04 +0900

    1

--- Chap03/src/UseVariables.java
+++ Chap03/src/UseVariables.java
@@ -4,6 +4,9 @@ public class UseVariables {
 		String name = "サトウ";
 		int num = 551;
 		double dec = 67.8;
+		char characte = 1;
+		byte testbyte = 200;
+		boolean checker = true;
 		
 		System.out.println(name);
 		System.out.println(num);
