653b2650c1b08babbc5abb68d096b1252eedb4b0
kohei0517 <losthound.kei127@gmail.com>
2023-11-29 19:52:12 +0900

    変数その１

---
+++ Chap03/src/UseVariables.java
@@ -0,0 +1,13 @@
+
+public class UseVariables {
+	public static void main(String[] args) {
+		String name = "サトウ";
+		int num = 551;
+		double dec = 67.8;
+		
+		System.out.println(name);
+		System.out.println(num);
+		System.out.println(dec);
+	}
+
+}
