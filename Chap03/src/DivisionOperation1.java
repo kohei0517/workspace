01c2ce14ad303ee4ddfccacf9c544af405b8475f
kohei0517 <losthound.kei127@gmail.com>
2023-12-03 22:14:51 +0900

    Division

---
+++ Chap03/src/DivisionOperation1.java
@@ -0,0 +1,11 @@
+
+public class DivisionOperation1 {
+	public static void main(String[] args) {
+		int num1 = 6;
+		int num2 = 5;
+		
+		int ans = num1/num2;
+		
+		System.out.println(ans);
+	}
+}
---
+++ Chap03/src/DivisionOperation2.java
@@ -0,0 +1,11 @@
+
+public class DivisionOperation2 {
+	public static void main(String[] args) {
+		int num1 = 6;
+		int num2 = 5;
+		
+		double ans = (double) num1 / num2;
+		
+		System.out.println(ans);
+	}
+}
