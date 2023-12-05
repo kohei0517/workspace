7f5c2aa74601e251d14507e333902e0b11e0bf08
kohei0517 <losthound.kei127@gmail.com>
2023-12-05 21:32:47 +0900

    chapter3-3

---
+++ Chap03/src/RemainderOperation.java
@@ -0,0 +1,11 @@
+
+public class RemainderOperation {
+	public static void main(String[] args) {
+		int num1 = 111;
+		int num2 = 10;
+		
+		int ans = num1 % num2;
+		
+		System.out.println(ans);
+	}
+}
