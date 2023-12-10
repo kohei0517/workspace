2cb3409f95f9508a470585a93781350bd4a84bfc
kohei0517 <losthound.kei127@gmail.com>
2023-12-10 21:20:12 +0900

    chap3-04

---
+++ Chap03/src/AssignmentOperator.java
@@ -0,0 +1,11 @@
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		int num = 346;
+		num = num + 4 ;
+		System.out.println("numの値:"+num);
+		
+		num += 5;
+		System.out.println("numの値:"+num);
+	}
+}
